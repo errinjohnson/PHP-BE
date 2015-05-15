c6dbb6dcec25ddf14d30ed3516eefa5e3278b1f9
Errin Johnson <errin.johnson@gmail.com>
2015-05-15 04:49:02 +0000

    added framework foundations, framework slick_slider, slider.html #33602425209457

--- index.php
+++ index.php
@@ -6,12 +6,15 @@
 <head>
     <title>Developers Project Manager</title>
     <link rel="stylesheet" href="myCss/application.css" type="text/css" />
+    <?php date_default_timezone_set('EST');?>
 </head>
 
 <body class="wrapper">
     <header>
         <div>
             <h1>Code Lousiville | Project-PHP</h1>
+            <!-- Asana tag#34267351905331 -->
+            <h2>Today is <?php echo date('l, F j, Y'); ?></h2>
         </div>
     </header>
 
