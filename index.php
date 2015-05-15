db4479ac5c734901690ad446118a30009c494b6a
Errin Johnson <errin.johnson@gmail.com>
2015-05-15 02:33:56 +0000

    added html structure to new Project-PHP #33602425209457#

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
+
 <!DOCTYPE html>
 <html>
+    <!--//Asana #33602425209457-->
 
 <head>
-    <title> </title>
+    <title>Developers Project Manager</title>
+    <link rel="stylesheet" href="myCss/application.css" type="text/css" />
 </head>
 
-<body>
-    <?php ?>
+<body class="wrapper">
+    <header>
+        <div>
+            <h1>Code Lousiville | Project-PHP</h1>
+        </div>
+    </header>
+
+    <footer>
+        <h4>Responsive Web Design | &copy;<?php echo date('Y'); ?> Alchemy Momentum</h4>
+    </footer>
 </body>
-
-
 
 </html>
