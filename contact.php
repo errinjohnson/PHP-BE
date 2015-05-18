e3a535aededb0b8809eb4d06747e21cfc625056c
Errin Johnson <errin.johnson@gmail.com>
2015-05-17 23:17:27 -0400

    initial commit for codelouisville php project

---
+++ contact.php
@@ -0,0 +1,7 @@
+<?php
+$pageTitle = "Contact Errin";
+$pageSection = "contact";
+include('inc/header.php'); ?>
+
+<?php include('inc/footer.php'); ?>
+<?php include('inc/script.php'); ?>
---
+++ portfolio.php
@@ -0,0 +1,7 @@
+<?php
+$pageTitle = "Portfolio Page";
+$pageSection = "portfolio";
+include('inc/header.php'); ?>
+<?php include('inc/slider.php'); ?>
+<?php include('inc/footer.php'); ?>
+<?php include('inc/script.php'); ?>
