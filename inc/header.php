61917e94bf3f5fadedacb623b541b156836810ec
Errin Johnson <errin.johnson@gmail.com>
2015-05-22 22:22:10 +0000

    updated footer and header inc files

--- inc/header.php
+++ inc/header.php
@@ -21,7 +21,7 @@
     <nav class="top-bar" data-topbar role="navigation">
         <ul class="title-area">
             <li class="name">
-                <h1><a href="index.php">Main Page</a></h1>
+                <h1><a href="http://www.alchemymomentum.com/">Alchemy Momentum</a></h1>
             </li>
             <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
             <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a>
