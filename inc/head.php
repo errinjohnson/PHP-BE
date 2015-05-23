cc511e07e45c8936bf40b9d44b2c1c7e0264c586
Errin Johnson <errin.johnson@gmail.com>
2015-05-23 01:52:22 +0000

    added contact form with | extra credit #33053744815471

--- inc/head.php
+++ inc/head.php
@@ -1,9 +1,11 @@
+<!DOCTYPE html>
+<html>
 <head>
-  <meta charset="utf-8">
-  <title>Front-End Developer | Wellness Entrepreneur</title>
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/_reset.css">
-  <link rel="stylesheet" href="css/main.css">
-  <link rel="stylesheet" href="css/responsive.css">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
- </head>
+    <title>
+        <?php echo $pageTitle; ?>
+    </title>
+    <link rel="stylesheet" href="foundation/css/foundation.css" />
+    <link rel="stylesheet" href="foundation-icons/foundation-icons.css" />
+    <link rel="stylesheet" href="myCss/application.css" type="text/css" />
+    <?php date_default_timezone_set( 'EST');?>
+</head>
