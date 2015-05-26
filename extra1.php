a941866dc557cae95a1a14dde42a56a89f87b58c
errinjohnson <errin.johnson@gmail.com>
2015-05-26 12:12:03 -0400

    testing git-ftp

--- extra1.php
+++ extra1.php
@@ -3,6 +3,6 @@ $pageTitle = "Extra Page One";
 $pageSection = "extra1";
 include('inc/head.php');
 include('inc/header.php'); ?>
-
+<div>testing git-ftp</div>
 <?php include('inc/footer.php'); ?>
 <?php include('inc/script.php'); ?>
