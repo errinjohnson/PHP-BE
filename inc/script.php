967c51e44d99ae75bb4028e0637f5e9192b0a3c9
errinjohnson <errin.johnson@gmail.com>
2015-06-03 16:36:23 -0400

    merge contact.php to one directory, linked pages project CL and AP

--- inc/script.php
+++ inc/script.php
@@ -1,12 +1,12 @@
 <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
     <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-    <script src="../js/vendor/jquery.js"></script>
-    <script src="../js/foundation.min.js"></script>
-    <script src="../js/foundation/foundation.dropdown.js"></script>
-    <script src="../js/foundation/foundation.topbar.js"></script>
-    <script src="../js/foundation/foundation.reveal.js"></script>
+    <script src="../frameworks/js/vendor/jquery.js"></script>
+    <script src="../frameworks/js/foundation/foundation.min.js"></script>
+    <script src="../frameworks/js/foundation/foundation.dropdown.js"></script>
+    <script src="../frameworks/js/foundation/foundation.topbar.js"></script>
+    <script src="../frameworks/js/foundation/foundation.reveal.js"></script>
     <script src="../frameworks/animsition-master/dist/js/jquery.animsition.min.js"></script>
-    <script src="js/myJs.js"></script>
+    <script src="#"></script>
 <script>
         $(document).foundation();
 </script>
